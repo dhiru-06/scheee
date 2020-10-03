9534285c0dde08490dd63c64287a40226550e374
dhiru-06 <65435462+dhiru-06@users.noreply.github.com>
2020-10-03 18:53:56 +0530

    Add files via upload

---
+++ build/precache-manifest.732f4dee9e3892a54e3a6bc1e37914e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e63aab4bbe6c869e3036b611e7761d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c6407e0131825c326e",
+    "url": "/static/css/main.e3b8ddee.chunk.css"
+  },
+  {
+    "revision": "12e8018543687cd35093",
+    "url": "/static/js/2.4b1dfbfe.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.4b1dfbfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c6407e0131825c326e",
+    "url": "/static/js/main.7504b5da.chunk.js"
+  },
+  {
+    "revision": "50dd553a58fba26d1234",
+    "url": "/static/js/runtime-main.520ef010.js"
+  },
+  {
+    "revision": "97591c7971970a545f72c92d80b5825e",
+    "url": "/static/media/back.97591c79.jpg"
+  }
+]);
